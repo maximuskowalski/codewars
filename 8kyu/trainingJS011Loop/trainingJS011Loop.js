5ce63a2ff651d039b93b6e5c7c13117b8c142f04
Max Kowalski <max@maxkow.com>
2022-05-05 14:57:41 +1000

    afternoon

---
+++ 8kyu/trainingJS011Loop/trainingJS011Loop.js
@@ -0,0 +1,13 @@
+function grabDoll(dolls){
+    let bag=[];
+    //coding here
+
+    return bag;
+}
+
+// Function Tests
+
+grabDoll(["Mickey Mouse","Hello Kitty","Snow white"]) // ["Hello Kitty"]);
+grabDoll(["Mickey Mouse","Hello Kitty","Hello Kitty","Snow white"]) //["Hello Kitty","Hello Kitty"]);
+grabDoll(["Mickey Mouse","Hello Kitty","Hello Kitty","Barbie doll","Snow white"]) //["Hello Kitty","Hello Kitty","Barbie doll"]);
+grabDoll(["Mickey Mouse","Barbie doll","Hello Kitty","Hello Kitty","Hello Kitty","Snow white"]) // ["Barbie doll","Hello Kitty","Hello Kitty"]);
