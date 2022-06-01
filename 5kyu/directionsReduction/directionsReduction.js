891b4cdf0f79ef7d6b9274405ddcbe707f727d54
Max Kowalski <max@maxkow.com>
2022-06-01 19:52:56 +1000

    check push

--- 5kyu/directionsReduction/directionsReduction.js
+++ 5kyu/directionsReduction/directionsReduction.js
@@ -1,21 +1,21 @@
-function dirReduc(arr){
+function dirReduc(arr) {
 
     let newArr = []
 
-    for( let i = 0; i < arr.length; i++ ){
-        if(    ( arr[i] === "NORTH" && arr[i+1] === "SOUTH" )
-            || ( arr[i] === "SOUTH" && arr[i+1] === "NORTH" )
-            || ( arr[i] === "EAST" && arr[i+1] === "WEST" )
-            || ( arr[i] == "WEST" && arr[i+1] === "EAST" )
-            ) {
-                i++
+    for (let i = 0; i < arr.length; i++) {
+        if ((arr[i] === "NORTH" && arr[i + 1] === "SOUTH") ||
+            (arr[i] === "SOUTH" && arr[i + 1] === "NORTH") ||
+            (arr[i] === "EAST" && arr[i + 1] === "WEST") ||
+            (arr[i] == "WEST" && arr[i + 1] === "EAST")
+        ) {
+            i++
 
-        }else{
-            if(    ( newArr[newArr.length - 1] === "NORTH" && arr[i] === "SOUTH" )
-                || ( newArr[newArr.length - 1] === "SOUTH" && arr[i] === "NORTH" )
-                || ( newArr[newArr.length - 1] === "EAST" && arr[i] === "WEST" )
-                || ( newArr[newArr.length - 1] === "WEST" && arr[i] === "EAST" )
-            ){
+        } else {
+            if ((newArr[newArr.length - 1] === "NORTH" && arr[i] === "SOUTH") ||
+                (newArr[newArr.length - 1] === "SOUTH" && arr[i] === "NORTH") ||
+                (newArr[newArr.length - 1] === "EAST" && arr[i] === "WEST") ||
+                (newArr[newArr.length - 1] === "WEST" && arr[i] === "EAST")
+            ) {
                 newArr.pop()
             } else {
                 newArr.push(arr[i])
